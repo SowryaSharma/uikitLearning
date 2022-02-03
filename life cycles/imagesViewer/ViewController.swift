7b291409906be735035c1b1dca52b8e48f733309
SowryaSharma <65005237+SowryaSharma@users.noreply.github.com>
2022-02-03 11:15:50 +0530

    Add files via upload

---
+++ life cycles/imagesViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imagesViewer
+//
+//  Created by Cumulations Technologies on 28/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+        
+
+
+
+    }
+}
+
